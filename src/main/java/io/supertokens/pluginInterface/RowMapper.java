c39714d8c28299a6017858f06d02bb52dd736927
Chotu Chaudhary <ankushchaudhary348@gmail.com>
2020-12-27 12:54:00 +0530

    WIP: Add RowMapper for reusing same in multiple sql plugin (#9)
    * Add RowMapper for reusing same in multiple sql plugin
    it can be reused in postgresql and mysql plugins
    
    * Add generic mapper interface for supporting Sql/NoSql db
    
    Co-authored-by: ankush.kumar3 <ankush.kumar3@olacabs.com>

---
+++ src/main/java/io/supertokens/pluginInterface/RowMapper.java
@@ -0,0 +1,15 @@
+package io.supertokens.pluginInterface;
+
+import io.supertokens.pluginInterface.exceptions.StorageQueryException;
+
+public interface RowMapper<P, Q> {
+    P map(Q rs) throws Exception;
+
+    default P mapOrThrow(Q rs) throws StorageQueryException {
+        try {
+            return map(rs);
+        } catch(Exception e) {
+            throw new StorageQueryException(e);
+        }
+    }
+}
