f945063ef47965e67bf82d210984e5451440bfe0
Rishabh Poddar <rishabh.poddar@gmail.com>
2021-02-09 20:43:38 +0530

    Thirdparty (#14)
    * creates object to hold userinfo
    
    * adds functions for signinup in third party
    
    * adds user get functions for third party
    
    * adds user pagination query
    
    * adds get user count query

---
+++ src/main/java/io/supertokens/pluginInterface/thirdparty/ThirdPartyStorage.java
@@ -0,0 +1,43 @@
+/*
+ *    Copyright (c) 2020, VRAI Labs and/or its affiliates. All rights reserved.
+ *
+ *    This software is licensed under the Apache License, Version 2.0 (the
+ *    "License") as published by the Apache Software Foundation.
+ *
+ *    You may not use this file except in compliance with the License. You may
+ *    obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *    License for the specific language governing permissions and limitations
+ *    under the License.
+ */
+
+package io.supertokens.pluginInterface.thirdparty;
+
+import io.supertokens.pluginInterface.Storage;
+import io.supertokens.pluginInterface.exceptions.StorageQueryException;
+import io.supertokens.pluginInterface.thirdparty.exception.DuplicateThirdPartyUserException;
+import io.supertokens.pluginInterface.thirdparty.exception.DuplicateUserIdException;
+
+import javax.annotation.Nonnull;
+
+public interface ThirdPartyStorage extends Storage {
+
+    void signUp(UserInfo userInfo)
+            throws StorageQueryException, DuplicateUserIdException, DuplicateThirdPartyUserException;
+
+    UserInfo getUserInfoUsingId(String thirdPartyId, String thirdPartyUserId)
+            throws StorageQueryException;
+
+    UserInfo getUserInfoUsingId(String userId) throws StorageQueryException;
+
+    UserInfo[] getUsers(@Nonnull String userId, @Nonnull Long timeJoined, @Nonnull Integer limit,
+                        @Nonnull String timeJoinedOrder) throws StorageQueryException;
+
+    UserInfo[] getUsers(@Nonnull Integer limit, @Nonnull String timeJoinedOrder) throws StorageQueryException;
+
+    long getUsersCount() throws StorageQueryException;
+
+}
