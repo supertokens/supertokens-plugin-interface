f1424c19f9cfd2791966df6c2cdde4f5d00d75f9
Mihaly Lengyel <mihaly.lengyel@tresorit.com>
2021-11-24 00:21:03 +0100

    Added auto-generated hashCode & equals to PWLessCode

--- src/main/java/io/supertokens/pluginInterface/passwordless/PasswordlessCode.java
+++ src/main/java/io/supertokens/pluginInterface/passwordless/PasswordlessCode.java
@@ -12,4 +12,47 @@ public class PasswordlessCode {
         this.linkCodeHash = linkCodeHash;
         this.createdAt = createdAt;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((createdAt == null) ? 0 : createdAt.hashCode());
+        result = prime * result + ((deviceIdHash == null) ? 0 : deviceIdHash.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((linkCodeHash == null) ? 0 : linkCodeHash.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        PasswordlessCode other = (PasswordlessCode) obj;
+        if (createdAt == null) {
+            if (other.createdAt != null)
+                return false;
+        } else if (!createdAt.equals(other.createdAt))
+            return false;
+        if (deviceIdHash == null) {
+            if (other.deviceIdHash != null)
+                return false;
+        } else if (!deviceIdHash.equals(other.deviceIdHash))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (linkCodeHash == null) {
+            if (other.linkCodeHash != null)
+                return false;
+        } else if (!linkCodeHash.equals(other.linkCodeHash))
+            return false;
+        return true;
+    }
 }
