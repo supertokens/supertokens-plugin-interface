d5be5ffa4988aae2d21d2149e22f3e9d51c29765
Joel Coutinho <joelsteve97@hotmail.com>
2023-02-14 16:28:58 +0530

    updates

--- src/main/java/io/supertokens/pluginInterface/dashboard/sqlStorage/DashboardSQLStorage.java
+++ src/main/java/io/supertokens/pluginInterface/dashboard/sqlStorage/DashboardSQLStorage.java
@@ -2,14 +2,15 @@ package io.supertokens.pluginInterface.dashboard.sqlStorage;
 
 import io.supertokens.pluginInterface.dashboard.DashboardStorage;
 import io.supertokens.pluginInterface.dashboard.exceptions.DuplicateEmailException;
+import io.supertokens.pluginInterface.dashboard.exceptions.UserIdNotFoundException;
 import io.supertokens.pluginInterface.exceptions.StorageQueryException;
 import io.supertokens.pluginInterface.sqlStorage.SQLStorage;
 import io.supertokens.pluginInterface.sqlStorage.TransactionConnection;
 
 public interface DashboardSQLStorage extends DashboardStorage, SQLStorage {
 
-    void updateDashboardUsersEmailWithUserId_Transaction(TransactionConnection con, String userId, String newEmail) throws StorageQueryException, DuplicateEmailException;
+    void updateDashboardUsersEmailWithUserId_Transaction(TransactionConnection con, String userId, String newEmail) throws StorageQueryException, DuplicateEmailException, UserIdNotFoundException;
 
-    void updateDashboardUsersPasswordWithUserId_Transaction(TransactionConnection con, String userId, String newPassword) throws StorageQueryException;
+    void updateDashboardUsersPasswordWithUserId_Transaction(TransactionConnection con, String userId, String newPassword) throws StorageQueryException, UserIdNotFoundException;
     
 }
