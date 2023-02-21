c73ed36cb84bac2114c8d75fdaa488354aab48a4
KShivendu <shivendu@iitbhilai.ac.in>
2023-02-21 13:09:46 +0530

    feat: Add optional deviceName to TOTPUsedCode

--- src/main/java/io/supertokens/pluginInterface/totp/TOTPUsedCode.java
+++ src/main/java/io/supertokens/pluginInterface/totp/TOTPUsedCode.java
@@ -1,13 +1,17 @@
 package io.supertokens.pluginInterface.totp;
 
+import javax.annotation.Nullable;
+
 public class TOTPUsedCode {
     public final String userId;
+    public final @Nullable String deviceName;
     public final String code;
     public final boolean isValidCode;
     public final long expiryTime;
 
-    public TOTPUsedCode(String userId, String code, Boolean isValidCode, long expiryTime) {
+    public TOTPUsedCode(String userId, @Nullable String deviceName, String code, Boolean isValidCode, long expiryTime) {
         this.userId = userId;
+        this.deviceName = deviceName;
         this.code = code;
         this.isValidCode = isValidCode;
         this.expiryTime = expiryTime;
