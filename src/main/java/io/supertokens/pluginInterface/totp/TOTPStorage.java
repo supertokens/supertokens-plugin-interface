ba5c2ef064eb0f70afaecc89f65b77daab697160
KShivendu <shivendu@iitbhilai.ac.in>
2023-03-09 16:32:47 +0530

    refactor: insertUsedCode and getAllusedCodes should be part of a transaction

--- src/main/java/io/supertokens/pluginInterface/totp/TOTPStorage.java
+++ src/main/java/io/supertokens/pluginInterface/totp/TOTPStorage.java
@@ -3,9 +3,7 @@ package io.supertokens.pluginInterface.totp;
 import io.supertokens.pluginInterface.nonAuthRecipe.NonAuthRecipeStorage;
 import io.supertokens.pluginInterface.exceptions.StorageQueryException;
 import io.supertokens.pluginInterface.totp.exception.DeviceAlreadyExistsException;
-import io.supertokens.pluginInterface.totp.exception.TotpNotEnabledException;
 import io.supertokens.pluginInterface.totp.exception.UnknownDeviceException;
-import io.supertokens.pluginInterface.totp.exception.UsedCodeAlreadyExistsException;
 
 public interface TOTPStorage extends NonAuthRecipeStorage {
         /** Create a new device and a new user if the user does not exist: */
@@ -23,17 +21,6 @@ public interface TOTPStorage extends NonAuthRecipeStorage {
 
         /** Get the devices for a user */
         TOTPDevice[] getDevices(String userId)
-                        throws StorageQueryException;
-
-        /** Insert a used TOTP code for an existing user: */
-        void insertUsedCode(TOTPUsedCode code)
-                        throws StorageQueryException, TotpNotEnabledException, UsedCodeAlreadyExistsException;
-
-        /**
-         * Get totp used codes for user (expired/non-expired) yet (sorted by descending
-         * order of created time):
-         */
-        TOTPUsedCode[] getAllUsedCodesDescOrder(String userId)
                         throws StorageQueryException;
 
         /** Remove expired codes from totp used codes for all users: */
--- src/main/java/io/supertokens/pluginInterface/totp/sqlStorage/TOTPSQLStorage.java
+++ src/main/java/io/supertokens/pluginInterface/totp/sqlStorage/TOTPSQLStorage.java
@@ -1,11 +1,12 @@
 package io.supertokens.pluginInterface.totp.sqlStorage;
 
-import java.sql.SQLException;
-
 import io.supertokens.pluginInterface.exceptions.StorageQueryException;
 import io.supertokens.pluginInterface.sqlStorage.SQLStorage;
 import io.supertokens.pluginInterface.sqlStorage.TransactionConnection;
 import io.supertokens.pluginInterface.totp.TOTPStorage;
+import io.supertokens.pluginInterface.totp.TOTPUsedCode;
+import io.supertokens.pluginInterface.totp.exception.TotpNotEnabledException;
+import io.supertokens.pluginInterface.totp.exception.UsedCodeAlreadyExistsException;
 
 public interface TOTPSQLStorage extends TOTPStorage, SQLStorage {
     public int deleteDevice_Transaction(TransactionConnection con, String userId, String deviceName)
@@ -16,5 +17,17 @@ public interface TOTPSQLStorage extends TOTPStorage, SQLStorage {
 
     public void removeUser_Transaction(TransactionConnection con, String userId)
             throws StorageQueryException;
+
+    /**
+     * Get totp used codes for user (expired/non-expired) yet (sorted by descending
+     * order of created time):
+     */
+    public TOTPUsedCode[] getAllUsedCodesDescOrderAndLockByUser_Transaction(TransactionConnection con,
+            String userId)
+            throws StorageQueryException;
+
+    /** Insert a used TOTP code for an existing user: */
+    void insertUsedCode_Transaction(TransactionConnection con, TOTPUsedCode code)
+            throws StorageQueryException, TotpNotEnabledException, UsedCodeAlreadyExistsException;
 
 }
