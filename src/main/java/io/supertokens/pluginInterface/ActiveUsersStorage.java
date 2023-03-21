d1ee62eebdb744b1c612b179f22cc7e34f64c70c
KShivendu <shivendu@iitbhilai.ac.in>
2023-03-21 12:59:26 +0530

    refactor: Throw error in storage Layer and later suppress them directly in API layer

--- src/main/java/io/supertokens/pluginInterface/ActiveUsersStorage.java
+++ src/main/java/io/supertokens/pluginInterface/ActiveUsersStorage.java
@@ -1,9 +1,11 @@
 package io.supertokens.pluginInterface;
 
+import io.supertokens.pluginInterface.exceptions.StorageQueryException;
+
 public interface ActiveUsersStorage extends Storage {
     /* Update the last active time of a user to now */
-    void updateLastActive(String userId);
+    void updateLastActive(String userId) throws StorageQueryException;
 
     /* Count the number of users who did some activity after given timestamp */
-    int countUsersActiveSince(long time);
+    int countUsersActiveSince(long time) throws StorageQueryException;
 }
