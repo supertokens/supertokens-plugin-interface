2703b900894a97814c5febcb81382acc26a666de
Adithya Bhat <adithya@quiph.com>
2023-06-07 11:45:25 +0530

    style: removed extra space

--- src/main/java/io/supertokens/pluginInterface/oauth2/exception/DuplicateOAuth2ClientNameException.java
+++ src/main/java/io/supertokens/pluginInterface/oauth2/exception/DuplicateOAuth2ClientNameException.java
@@ -16,6 +16,6 @@
 
 package io.supertokens.pluginInterface.oauth2.exception;
 
-public class DuplicateOAuth2ClientNameException extends Exception{
+public class DuplicateOAuth2ClientNameException extends Exception {
     private static final long serialVersionUID = 6848053563771647272L;
 }
