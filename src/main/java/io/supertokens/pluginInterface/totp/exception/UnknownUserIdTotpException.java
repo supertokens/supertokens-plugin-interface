b2b270ecdd6b7c670a59b24c44aa3e1fb4a9efbc
KShivendu <kshivendu1@gmail.com>
2023-06-28 15:12:36 +0530

    Replace TotpNotEnabledError with UnknownUserIdTotpError

--- src/main/java/io/supertokens/pluginInterface/totp/exception/UnknownUserIdTotpException.java
+++ src/main/java/io/supertokens/pluginInterface/totp/exception/UnknownUserIdTotpException.java
@@ -1,5 +1,5 @@
 package io.supertokens.pluginInterface.totp.exception;
 
-public class UnknownUserIdException extends Exception {
+public class UnknownUserIdTotpException extends Exception {
     private static final long serialVersionUID = 6848053563771647272L;
 }
