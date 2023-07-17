da001ec199f0634d6cdaae8b1223912990b2d670
rishabhpoddar <rishabh.poddar@gmail.com>
2023-07-17 20:14:11 +0530

    changes to interface to make it per tenant

--- src/main/java/io/supertokens/pluginInterface/authRecipe/sqlStorage/AuthRecipeSQLStorage.java
+++ src/main/java/io/supertokens/pluginInterface/authRecipe/sqlStorage/AuthRecipeSQLStorage.java
@@ -20,26 +20,37 @@ import io.supertokens.pluginInterface.authRecipe.AuthRecipeStorage;
 import io.supertokens.pluginInterface.authRecipe.AuthRecipeUserInfo;
 import io.supertokens.pluginInterface.exceptions.StorageQueryException;
 import io.supertokens.pluginInterface.multitenancy.AppIdentifier;
+import io.supertokens.pluginInterface.multitenancy.TenantIdentifier;
+import io.supertokens.pluginInterface.sqlStorage.SQLStorage;
+import io.supertokens.pluginInterface.sqlStorage.TransactionConnection;
 
-public interface AuthRecipeSQLStorage extends AuthRecipeStorage {
+public interface AuthRecipeSQLStorage extends AuthRecipeStorage, SQLStorage {
 
-    // These are all app indetifier cause they are used in account linking which link users
-    // across tenants anyway.
-    AuthRecipeUserInfo getPrimaryUserById_Transaction(AppIdentifier appIdentifier, String userId)
+    AuthRecipeUserInfo getPrimaryUserById_Transaction(AppIdentifier appIdentifier, TransactionConnection con,
+                                                      String userId)
             throws StorageQueryException;
 
     // lock order:
     // - emailpassword table
     // - thirdparty table
     // - passwordless table
-    AuthRecipeUserInfo[] listPrimaryUsersByEmail_Transaction(AppIdentifier appIdentifier, String email)
+    AuthRecipeUserInfo[] listPrimaryUsersByEmail_Transaction(TenantIdentifier tenantIdentifier,
+                                                             TransactionConnection con,
+                                                             String email)
             throws StorageQueryException;
 
     // locks only passwordless table
-    AuthRecipeUserInfo[] listPrimaryUsersByPhoneNumber_Transaction(AppIdentifier appIdentifier, String phoneNumber)
+    AuthRecipeUserInfo[] listPrimaryUsersByPhoneNumber_Transaction(TenantIdentifier tenantIdentifier,
+                                                                   TransactionConnection con, String phoneNumber)
             throws StorageQueryException;
 
     // locks on thirdparty table
-    AuthRecipeUserInfo getPrimaryUserByThirdPartyInfo_Transaction(AppIdentifier appIdentifier, String thirdPartyId,
-                                                                  String thirdPartyUserId) throws StorageQueryException;
+    AuthRecipeUserInfo getPrimaryUsersByThirdPartyInfo_Transaction(TenantIdentifier tenantIdentifier,
+                                                                   TransactionConnection con, String thirdPartyId,
+                                                                   String thirdPartyUserId)
+            throws StorageQueryException;
+
+    void makePrimaryUser_Transaction(AppIdentifier appIdentifier, TransactionConnection con, String userId)
+            throws StorageQueryException;
+
 }
