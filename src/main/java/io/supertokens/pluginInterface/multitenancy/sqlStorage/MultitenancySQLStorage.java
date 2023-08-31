8fa5dbb9b6941da206de8d1389176fbf99439230
Sattvik Chakravarthy <sattvik@supertokens.com>
2023-08-31 11:54:03 +0530

    Al user tenant association (#115)
    * fix: multitenancy changes
    
    * fix: addUserIdToTenant to transaction

---
+++ src/main/java/io/supertokens/pluginInterface/multitenancy/sqlStorage/MultitenancySQLStorage.java
@@ -0,0 +1,34 @@
+/*
+ *    Copyright (c) 2023, VRAI Labs and/or its affiliates. All rights reserved.
+ *
+ *    This software is licensed under the Apache License, Version 2.0 (the
+ *    "License") as published by the Apache Software Foundation.
+ *
+ *    You may not use this file except in compliance with the License. You may
+ *    obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *    License for the specific language governing permissions and limitations
+ *    under the License.
+ */
+
+package io.supertokens.pluginInterface.multitenancy.sqlStorage;
+
+import io.supertokens.pluginInterface.emailpassword.exceptions.DuplicateEmailException;
+import io.supertokens.pluginInterface.emailpassword.exceptions.UnknownUserIdException;
+import io.supertokens.pluginInterface.exceptions.StorageQueryException;
+import io.supertokens.pluginInterface.multitenancy.MultitenancyStorage;
+import io.supertokens.pluginInterface.multitenancy.TenantIdentifier;
+import io.supertokens.pluginInterface.multitenancy.exceptions.TenantOrAppNotFoundException;
+import io.supertokens.pluginInterface.passwordless.exception.DuplicatePhoneNumberException;
+import io.supertokens.pluginInterface.sqlStorage.SQLStorage;
+import io.supertokens.pluginInterface.sqlStorage.TransactionConnection;
+import io.supertokens.pluginInterface.thirdparty.exception.DuplicateThirdPartyUserException;
+
+public interface MultitenancySQLStorage extends MultitenancyStorage, SQLStorage {
+    boolean addUserIdToTenant_Transaction(TenantIdentifier tenantIdentifier, TransactionConnection con, String userId)
+            throws StorageQueryException, TenantOrAppNotFoundException, DuplicateEmailException,
+            DuplicateThirdPartyUserException, DuplicatePhoneNumberException, UnknownUserIdException;
+}
