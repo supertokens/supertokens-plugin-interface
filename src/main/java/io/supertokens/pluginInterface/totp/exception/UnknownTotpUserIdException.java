93497e8ae246bbce8bd0860cfb75314a1c50b8e8
Sattvik Chakravarthy <sattvik@gmail.com>
2023-09-28 12:32:07 +0530

    fix: rename exception

--- src/main/java/io/supertokens/pluginInterface/totp/exception/UnknownTotpUserIdException.java
+++ src/main/java/io/supertokens/pluginInterface/totp/exception/UnknownTotpUserIdException.java
@@ -1,5 +1,5 @@
 package io.supertokens.pluginInterface.totp.exception;
 
-public class UnknownUserIdTotpException extends Exception {
+public class UnknownTotpUserIdException extends Exception {
     private static final long serialVersionUID = 6848053563771647272L;
 }
--- src/main/java/io/supertokens/pluginInterface/totp/sqlStorage/TOTPSQLStorage.java
+++ src/main/java/io/supertokens/pluginInterface/totp/sqlStorage/TOTPSQLStorage.java
@@ -10,7 +10,7 @@ import io.supertokens.pluginInterface.totp.TOTPDevice;
 import io.supertokens.pluginInterface.totp.TOTPStorage;
 import io.supertokens.pluginInterface.totp.TOTPUsedCode;
 import io.supertokens.pluginInterface.totp.exception.UsedCodeAlreadyExistsException;
-import io.supertokens.pluginInterface.totp.exception.UnknownUserIdTotpException;
+import io.supertokens.pluginInterface.totp.exception.UnknownTotpUserIdException;
 
 public interface TOTPSQLStorage extends TOTPStorage, SQLStorage {
     public int deleteDevice_Transaction(TransactionConnection con, AppIdentifier appIdentifier, String userId,
@@ -38,7 +38,7 @@ public interface TOTPSQLStorage extends TOTPStorage, SQLStorage {
      * Insert a used TOTP code for an existing user:
      */
     void insertUsedCode_Transaction(TransactionConnection con, TenantIdentifier tenantIdentifier, TOTPUsedCode code)
-            throws StorageQueryException, UnknownUserIdTotpException, UsedCodeAlreadyExistsException,
+            throws StorageQueryException, UnknownTotpUserIdException, UsedCodeAlreadyExistsException,
             TenantOrAppNotFoundException;
 
 }
