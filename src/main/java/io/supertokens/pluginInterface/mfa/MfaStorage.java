5ca1977a97a9aa96f09feafac62de2b7159ffd97
Sattvik Chakravarthy <sattvik@gmail.com>
2023-10-12 15:48:03 +0530

    fix: mfa cleanup

--- src/main/java/io/supertokens/pluginInterface/mfa/MfaStorage.java
+++ src/main/java/io/supertokens/pluginInterface/mfa/MfaStorage.java
@@ -7,15 +7,4 @@ import io.supertokens.pluginInterface.nonAuthRecipe.NonAuthRecipeStorage;
 import io.supertokens.pluginInterface.sqlStorage.TransactionConnection;
 
 public interface MfaStorage extends NonAuthRecipeStorage {
-    // Enable (insert) a factor for a user and return true if it actually inserted something.
-    boolean enableFactor(TenantIdentifier tenantIdentifier, String userId, String factorId) throws StorageQueryException;
-
-    // List all the factors for a user:
-    String[] listFactors(TenantIdentifier tenantIdentifier, String userId) throws StorageQueryException;
-
-    // Disable (delete) a factor for a user and return true if it actually deleted something.
-    boolean disableFactor(TenantIdentifier tenantIdentifier, String userId, String factorId) throws StorageQueryException;
-
-    // Delete a user from a tenant (with all the relevant factors)
-    boolean deleteMfaInfoForUser(TenantIdentifier tenantIdentifier, String userId) throws StorageQueryException;
 }
