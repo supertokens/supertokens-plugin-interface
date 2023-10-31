6420672ce8e03ed639cbb791008ad2d7686d2dd5
Sattvik Chakravarthy <sattvik@gmail.com>
2023-10-31 16:50:52 +0530

    fix: mfa stats

--- src/main/java/io/supertokens/pluginInterface/ActiveUsersStorage.java
+++ src/main/java/io/supertokens/pluginInterface/ActiveUsersStorage.java
@@ -3,7 +3,6 @@ package io.supertokens.pluginInterface;
 import io.supertokens.pluginInterface.exceptions.StorageQueryException;
 import io.supertokens.pluginInterface.multitenancy.AppIdentifier;
 import io.supertokens.pluginInterface.nonAuthRecipe.NonAuthRecipeStorage;
-import io.supertokens.pluginInterface.sqlStorage.TransactionConnection;
 
 public interface ActiveUsersStorage extends NonAuthRecipeStorage {
     /* Update the last active time of a user to now */
@@ -12,17 +11,7 @@ public interface ActiveUsersStorage extends NonAuthRecipeStorage {
     /* Count the number of users who did some activity after given timestamp */
     int countUsersActiveSince(AppIdentifier appIdentifier, long time) throws StorageQueryException;
 
-    /* Count the number of users who have enabled TOTP */
-    int countUsersEnabledTotp(AppIdentifier appIdentifier) throws StorageQueryException;
-
-    /* Count the number of users who have enabled TOTP and are active */
-    int countUsersEnabledTotpAndActiveSince(AppIdentifier appIdentifier, long time) throws StorageQueryException;
-
-    /* Count the number of users who have enabled MFA */
-    int countUsersEnabledMfa(AppIdentifier appIdentifier) throws StorageQueryException;
-
-    /* Count the number of users who have enabled MFA and are active */
-    int countUsersEnabledMfaAndActiveSince(AppIdentifier appIdentifier, long time) throws StorageQueryException;
-
     int countUsersThatHaveMoreThanOneLoginMethodAndActiveSince(AppIdentifier appIdentifier, long sinceTime) throws StorageQueryException;
+
+    int countUsersThatHaveMoreThanOneLoginMethodOrTOTPEnabledAndActiveSince(AppIdentifier appIdentifier, long timestamp) throws StorageQueryException;
 }
