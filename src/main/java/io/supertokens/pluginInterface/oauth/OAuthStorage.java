a22c1320d078786320f1c688685e05453f7ca91b
Sattvik Chakravarthy <sattvik@supertokens.com>
2024-10-04 10:54:20 +0530

    feat: OAuth provider support (#164)
    * fix: tenant config
    
    * fix: thirdparty providers
    
    * fix: cleanup
    
    * fix: providers non null
    
    * fix: changelog
    
    * fix: pr comments
    
    * reformats code
    
    * adds function to check if client exists for the app
    
    * feat: oauth - WIP (#158)
    
    * feat: oauth recipe id, sqlstoreage and response type
    
    * fix: review fixes
    
    * fix: review fixes
    
    * fix: review fix - renamed exception
    
    * feat: OAuthStorage isClientAlreadyExists
    
    * feat: delete from oauth table
    
    * fix: removing unused/unnecessary method, change return type of other
    
    * fix: merge with latest (#159)
    
    * adding dev-v6.2.0 tag to this commit to ensure building
    
    * adding dev-v6.2.0 tag to this commit to ensure building
    
    * fix: listClientsForApp
    
    * fix: oauth plugin interface updates (#160)
    
    * fix: revoke
    
    * fix: pr comments
    
    * fix: oauth storage
    
    * fix: update
    
    * fix: add m2m token
    
    * fix: revoke and cleanup
    
    * fix: logout (#161)
    
    * fix: logout
    
    * fix: session revoke in logout
    
    * fix: version and cleanup
    
    * fix: rename / refactor
    
    * fix: changelog
    
    * fix: changelog
    
    * fix: constraints
    
    ---------
    
    Co-authored-by: rishabhpoddar <rishabh.poddar@gmail.com>
    Co-authored-by: Tamas Soltesz <solt.tamas@gmail.com>

---
+++ src/main/java/io/supertokens/pluginInterface/oauth/OAuthStorage.java
@@ -0,0 +1,63 @@
+/*
+ *    Copyright (c) 2024, VRAI Labs and/or its affiliates. All rights reserved.
+ *
+ *    This software is licensed under the Apache License, Version 2.0 (the
+ *    "License") as published by the Apache Software Foundation.
+ *
+ *    You may not use this file except in compliance with the License. You may
+ *    obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *    License for the specific language governing permissions and limitations
+ *    under the License.
+ */
+
+package io.supertokens.pluginInterface.oauth;
+
+import io.supertokens.pluginInterface.exceptions.StorageQueryException;
+import io.supertokens.pluginInterface.multitenancy.AppIdentifier;
+import io.supertokens.pluginInterface.multitenancy.exceptions.TenantOrAppNotFoundException;
+import io.supertokens.pluginInterface.nonAuthRecipe.NonAuthRecipeStorage;
+import io.supertokens.pluginInterface.oauth.exception.DuplicateOAuthLogoutChallengeException;
+import io.supertokens.pluginInterface.oauth.exception.OAuthClientNotFoundException;
+
+import java.util.List;
+
+public interface OAuthStorage extends NonAuthRecipeStorage {
+
+    public boolean doesOAuthClientIdExist(AppIdentifier appIdentifier, String clientId) throws
+            StorageQueryException;
+
+    public void addOrUpdateOauthClient(AppIdentifier appIdentifier, String clientId, boolean isClientCredentialsOnly) throws TenantOrAppNotFoundException, StorageQueryException;
+
+    public boolean deleteOAuthClient(AppIdentifier appIdentifier, String clientId) throws StorageQueryException;
+
+    public List<String> listOAuthClients(AppIdentifier appIdentifier) throws StorageQueryException;
+
+    public void revokeOAuthTokensBasedOnTargetFields(AppIdentifier appIdentifier, OAuthRevokeTargetType targetType, String targetValue, long exp) throws TenantOrAppNotFoundException, StorageQueryException;
+
+    public boolean isOAuthTokenRevokedBasedOnTargetFields(AppIdentifier appIdentifier, OAuthRevokeTargetType[] targetTypes, String[] targetValues, long issuedAt) throws StorageQueryException;
+
+    public void addOAuthM2MTokenForStats(AppIdentifier appIdentifier, String clientId, long iat, long exp) throws OAuthClientNotFoundException, StorageQueryException;
+
+    public void cleanUpExpiredAndRevokedOAuthTokensList() throws StorageQueryException;
+
+    public void addOAuthLogoutChallenge(AppIdentifier appIdentifier, String challenge, String clientId, String postLogoutRedirectionUri, String sessionHandle, String state, long timeCreated) throws
+            DuplicateOAuthLogoutChallengeException, OAuthClientNotFoundException, StorageQueryException;
+
+    public OAuthLogoutChallenge getOAuthLogoutChallenge(AppIdentifier appIdentifier, String challenge) throws StorageQueryException;
+
+    public void deleteOAuthLogoutChallenge(AppIdentifier appIdentifier, String challenge) throws StorageQueryException;
+
+    public void deleteOAuthLogoutChallengesBefore(long time) throws StorageQueryException;
+
+    public int countTotalNumberOfOAuthClients(AppIdentifier appIdentifier) throws StorageQueryException;
+
+    public int countTotalNumberOfClientCredentialsOnlyOAuthClients(AppIdentifier appIdentifier) throws StorageQueryException;
+
+    public int countTotalNumberOfOAuthM2MTokensCreatedSince(AppIdentifier appIdentifier, long since) throws StorageQueryException;
+
+    public int countTotalNumberOfOAuthM2MTokensAlive(AppIdentifier appIdentifier) throws StorageQueryException;
+}
