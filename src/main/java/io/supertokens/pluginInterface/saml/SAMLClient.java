ac6c7bf0ca4aca5ea185d773fcc488cf5072bd41
Sattvik Chakravarthy <sattvik@gmail.com>
2025-09-16 18:48:12 +0530

    fix: saml interfaces

---
+++ src/main/java/io/supertokens/pluginInterface/saml/SAMLClient.java
@@ -0,0 +1,55 @@
+/*
+ *    Copyright (c) 2025, VRAI Labs and/or its affiliates. All rights reserved.
+ *
+ *    This software is licensed under the Apache License, Version 2.0 (the
+ *    "License") as published by the Apache Software Foundation.
+ *
+ *    You may not use this file except in compliance with the License. You may
+ *    obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *    License for the specific language governing permissions and limitations
+ *    under the License.
+ *
+ */
+
+package io.supertokens.pluginInterface.saml;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+
+import java.util.List;
+
+public class SAMLClient {
+    public final String clientId;
+    public final String ssoLoginURL;
+    public final JsonArray redirectURIs;
+    public final String defaultRedirectURI;
+    public final String spEntityId;
+
+    public SAMLClient(String clientId, String ssoLoginURL, JsonArray redirectURIs, String defaultRedirectURI, String spEntityId) {
+        this.clientId = clientId;
+        this.ssoLoginURL = ssoLoginURL;
+        this.redirectURIs = redirectURIs;
+        this.defaultRedirectURI = defaultRedirectURI;
+        this.spEntityId = spEntityId;
+    }
+
+    public JsonObject toJson() {
+        JsonObject res = new JsonObject();
+
+        res.addProperty("clientId", this.clientId);
+        res.addProperty("defaultRedirectURI", this.defaultRedirectURI);
+        res.add("redirectURIs", redirectURIs);
+
+        if (this.spEntityId != null) {
+            res.addProperty("spEntityId", spEntityId);
+        }
+
+        return res;
+    }
+}
